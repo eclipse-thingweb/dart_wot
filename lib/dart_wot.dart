61e1ba9dfaeeb4cc4043748897f18635a687b04f
Jan Romann <jan.romann@uni-bremen.de>
2021-12-16 17:01:03 +0100

    feat(lib): Define library exports

--- lib/dart_wot.dart
+++ lib/dart_wot.dart
@@ -1,8 +1,24 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Copyright 2021 The NAMIB Project Developers
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// A W3C Web of Things implementation written in Dart.
 library dart_wot;
 
-export 'src/dart_wot_base.dart';
+/// Protocol Bindings
+export 'binding_coap.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+/// Core implementation providing WoT servient.
+export 'core.dart';
+
+/// Thing Description and Thing Model Definitions
+export 'definitions.dart';
+
+/// Provides a WoT Scripting API implementation
+export 'scripting_api.dart';
