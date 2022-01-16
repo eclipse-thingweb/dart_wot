b6c79a738fb50a8b043b04f00e1887246b2d0e15
Jan Romann <jan.romann@uni-bremen.de>
2022-01-16 06:36:43 +0100

    feat(definitions): add DataSchema fields to Event class

--- lib/src/definitions/interaction_affordances/event.dart
+++ lib/src/definitions/interaction_affordances/event.dart
@@ -8,16 +8,34 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+import '../data_schema.dart';
 import '../form.dart';
 import 'interaction_affordance.dart';
 
 /// Class representing an [Event] Affordance in a Thing Description.
 class Event extends InteractionAffordance {
+  /// Defines data that needs to be passed upon [subscription].
+  DataSchema? subscription;
+
+  /// Defines the [DataSchema] of the Event instance messages pushed by the
+  /// Thing.
+  DataSchema? data;
+
+  /// Defines any data that needs to be passed to cancel a subscription.
+  DataSchema? cancellation;
+
   /// Creates a new [Event] from a [List] of [forms].
   Event(List<Form> forms) : super(forms);
 
   /// Creates a new [Event] from a [json] object.
   Event.fromJson(Map<String, dynamic> json) : super([]) {
     parseAffordanceFields(json);
+    _parseEventFields(json);
+  }
+
+  void _parseEventFields(Map<String, dynamic> json) {
+    subscription = DataSchema.fromJson(json);
+    data = DataSchema.fromJson(json);
+    cancellation = DataSchema.fromJson(json);
   }
 }
