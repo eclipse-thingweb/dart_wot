f0db171e940f0930ed869f29c0cd4c95c2417e62
Jan Romann <jan.romann@uni-bremen.de>
2022-03-14 01:16:55 +0100

    refactor: use simplified cbor API in codec

--- lib/src/core/codecs/cbor_codec.dart
+++ lib/src/core/codecs/cbor_codec.dart
@@ -4,76 +4,27 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:cbor/cbor.dart';
-import 'package:typed_data/typed_data.dart';
+import 'package:cbor/cbor.dart' as cbor;
 
 import '../../definitions/data_schema.dart';
 import 'content_codec.dart';
 
 /// A [ContentCodec] that encodes and decodes CBOR data.
 class CborCodec extends ContentCodec {
-  final Cbor _cborConverter = Cbor();
-
   @override
   ByteBuffer valueToBytes(
       Object? value, DataSchema? dataSchema, Map<String, String>? parameters) {
-    // TODO(JKRhb): Make sure that exceptions are handled.
-    // TODO(JKRhb): Should more data types be supported? (Probably not I guess.)
-
-    if (value == null) {
-      return Uint8List(0).buffer;
-    } else {
-      var success = true;
-
-      if (value is Map) {
-        success = _cborConverter.encoder.writeMap(value);
-      } else if (value is int) {
-        _cborConverter.encoder.writeInt(value);
-      } else if (value is double) {
-        _cborConverter.encoder.writeFloat(value);
-      } else if (value is String) {
-        _cborConverter.encoder.writeString(value);
-      } else if (value is bool) {
-        _cborConverter.encoder.writeBool(value);
-      } else if (value is List) {
-        success = _cborConverter.encoder.writeArray(value);
-      } else {
-        throw UnsupportedError(
-            "CBOR encoder encountered invalid unsupported type "
-            "${value.runtimeType}.");
-      }
-
-      if (!success) {
-        throw ArgumentError(
-            "CBOR encoding of data type ${value.runtimeType} failed!");
-      }
-
-      final bytes = _cborConverter.output.getData();
-      _cborConverter.clearDecodeStack();
-      return bytes.buffer;
-    }
+    final result = cbor.cborEncode(cbor.CborValue(value));
+    return Uint8List.fromList(result).buffer;
   }
 
   @override
   Object? bytesToValue(ByteBuffer bytes, DataSchema? dataSchema,
       Map<String, String>? parameters) {
     // TODO(JKRhb): Use dataSchema for validation
-    // TODO(JKRhb): Check if there is a more elegant way for dealing with CBOR
-    //              decoding
-
-    final buffer = Uint8Buffer()..addAll(bytes.asUint8List());
-
-    _cborConverter.decodeFromBuffer(buffer);
-    final jsonString = _cborConverter.decodedToJSON();
-    _cborConverter.clearDecodeStack();
-
-    if (jsonString != null) {
-      return jsonDecode(jsonString);
-    } else {
-      return null;
-    }
+    final result = cbor.cborDecode(bytes.asUint8List().toList(growable: false));
+    return result.toObject();
   }
 }
