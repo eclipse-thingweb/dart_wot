b6ecb980a609e3c6555c3d78e7ce14f80a0f7d59
Jan Romann <jan.romann@uni-bremen.de>
2022-03-21 16:56:01 +0100

    feat: add README example to example folder

---
+++ example/example.dart
@@ -0,0 +1,42 @@
+// Copyright 2022 The NAMIB Project Developers. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+import 'package:dart_wot/dart_wot.dart';
+
+Future<void> main(List<String> args) async {
+  final CoapClientFactory coapClientFactory = CoapClientFactory(null);
+  final servient = Servient()..addClientFactory(coapClientFactory);
+  final wot = await servient.start();
+
+  final thingDescriptionJson = '''
+  {
+    "@context": "http://www.w3.org/ns/td",
+    "title": "Test Thing",
+    "base": "coap://coap.me",
+    "security": ["nosec_sc"],
+    "securityDefinitions": {
+      "nosec_sc": {
+        "scheme": "nosec"
+      }
+    },
+    "properties": {
+      "status": {
+        "forms": [
+          {
+            "href": "/hello"
+          }
+        ]
+      }
+    }
+  }
+  ''';
+
+  final thingDescription = ThingDescription(thingDescriptionJson);
+  final consumedThing = await wot.consume(thingDescription);
+  final status = await consumedThing.readProperty("status", null);
+  final value = await status.value();
+  print(value);
+}
