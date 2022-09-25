08e5dbaaea1679fdcf84757d1a783e8c37e631e5
Jan Romann <jan.romann@uni-bremen.de>
2022-09-26 01:27:41 +0200

    refactor: refactor JSON parser methods

--- lib/src/definitions/extensions/json_parser.dart
+++ lib/src/definitions/extensions/json_parser.dart
@@ -2,6 +2,11 @@ import '../validation/validation_exception.dart';
 
 /// Extension for parsing fields of JSON objects.
 extension ParseField on Map<String, dynamic> {
+  dynamic _processFieldName(String name, Set<String>? parsedFields) {
+    parsedFields?.add(name);
+    return this[name];
+  }
+
   /// Parses a single field with a given [name].
   ///
   /// Ensures that the field value is of type [T] and returns `null` if the
@@ -10,10 +15,10 @@ extension ParseField on Map<String, dynamic> {
   /// If a [Set] of [parsedFields] is passed to this function, the field [name]
   /// will added. This can be used for filtering when parsing additional fields.
   T? parseField<T>(String name, [Set<String>? parsedFields]) {
-    final value = this[name];
-    parsedFields?.add(name);
-    if (value is T) {
-      return value;
+    final fieldValue = _processFieldName(name, parsedFields);
+
+    if (fieldValue is T) {
+      return fieldValue;
     }
 
     return null;
@@ -26,16 +31,16 @@ extension ParseField on Map<String, dynamic> {
   /// Like [parseField], it adds the field [name] to the set of [parsedFields],
   /// if present.
   T parseRequiredField<T>(String name, [Set<String>? parsedFields]) {
-    final value = parseField(name, parsedFields);
+    final fieldValue = parseField(name, parsedFields);
 
-    if (value is! T) {
+    if (fieldValue is! T) {
       throw ValidationException(
         'Value for field $name has wrong data type or is missing. '
-        'Expected ${T.runtimeType}, got ${value.runtimeType}.',
+        'Expected ${T.runtimeType}, got ${fieldValue.runtimeType}.',
       );
     }
 
-    return value;
+    return fieldValue;
   }
 
   /// Parses a map field with a given [name].
@@ -46,13 +51,12 @@ extension ParseField on Map<String, dynamic> {
   /// If a [Set] of [parsedFields] is passed to this function, the field [name]
   /// will added. This can be used for filtering when parsing additional fields.
   Map<String, T>? parseMapField<T>(String name, [Set<String>? parsedFields]) {
-    final dynamic mapField = this[name];
-    parsedFields?.add(name);
+    final fieldValue = _processFieldName(name, parsedFields);
 
-    if (mapField is Map<String, dynamic>) {
+    if (fieldValue is Map<String, dynamic>) {
       final Map<String, T> result = {};
 
-      for (final entry in mapField.entries) {
+      for (final entry in fieldValue.entries) {
         final value = entry.value;
         if (value is T) {
           result[entry.key] = value;
@@ -73,13 +77,12 @@ extension ParseField on Map<String, dynamic> {
   /// If a [Set] of [parsedFields] is passed to this function, the field [name]
   /// will added. This can be used for filtering when parsing additional fields.
   List<T>? parseArrayField<T>(String name, [Set<String>? parsedFields]) {
-    final value = this[name];
-    parsedFields?.add(name);
+    final fieldValue = parseField(name, parsedFields);
 
-    if (value is T) {
-      return [value];
-    } else if (value is List<dynamic>) {
-      return value.whereType<T>().toList(growable: false);
+    if (fieldValue is T) {
+      return [fieldValue];
+    } else if (fieldValue is List<dynamic>) {
+      return fieldValue.whereType<T>().toList(growable: false);
     }
 
     return null;
