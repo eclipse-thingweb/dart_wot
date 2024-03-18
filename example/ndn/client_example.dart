c8b3eb3c6e945df715ec4b4a2c30cb7da2f8ce3d
Jan Romann <jan.romann@hs-emden-leer.de>
2024-03-18 20:32:10 +0100

    fixup! feat: add initial NDN binding implementation

--- example/ndn/client_example.dart
+++ example/ndn/client_example.dart
@@ -8,7 +8,45 @@
 
 import "package:dart_wot/binding_ndn.dart";
 import "package:dart_wot/core.dart";
+import "package:dart_wot/src/binding_ndn/ndn_config.dart";
 
 Future<void> main() async {
-  final servient = Servient(clientFactories: [NdnClientFactory(ndnConfig)]);
+  final faceUri = Uri.parse("tcp4://localhost:6363");
+  final ndnConfig = NdnConfig(
+    faceUri: faceUri,
+  );
+
+  final servient = Servient(
+    clientFactories: [NdnClientFactory(ndnConfig: ndnConfig)],
+  );
+
+  final wot = await servient.start();
+
+  final thingDescription = {
+    "@context": "https://www.w3.org/2022/wot/td/v1.1",
+    "title": "NDN Thing",
+    "id": "urn:test",
+    "securityDefinitions": {
+      "nosec_sc": {"scheme": "nosec"},
+    },
+    "security": "nosec_sc",
+    "properties": {
+      "ping": {
+        "forms": [
+          {
+            "contentType": "text/plain",
+            "href": "ndn:///ndn/ping/9001",
+          },
+        ],
+      },
+    },
+  }.toThingDescription();
+
+  final consumedThing = await wot.consume(thingDescription);
+
+  final result = await consumedThing.readProperty("ping");
+
+  print(await result.value());
+
+  await servient.shutdown();
 }
