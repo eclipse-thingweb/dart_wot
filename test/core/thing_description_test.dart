4d4e0f7a52597cf186e42a503de2a619fc7758e8
Jan Romann <jan.romann@hs-emden-leer.de>
2024-05-18 12:38:33 +0200

    feat!: rework TD @context handling

--- test/core/thing_description_test.dart
+++ test/core/thing_description_test.dart
@@ -57,4 +57,22 @@ void main() {
       throwsA(isA<ValidationException>()),
     );
   });
+
+  test("use the correct @context entry as the default prefix value", () {
+    final thingDescription = {
+      "@context": [
+        "https://www.w3.org/2022/wot/td/v1.1",
+      ],
+      "title": "Invalid TD with missing security field.",
+      "security": "nosec_sc",
+      "securityDefinitions": {
+        "nosec_sc": {"scheme": "nosec"},
+      },
+    }.toThingDescription();
+
+    expect(
+      thingDescription.prefixMapping.defaultPrefixValue,
+      "https://www.w3.org/2022/wot/td/v1.1",
+    );
+  });
 }
